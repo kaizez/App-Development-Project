997e38187a25f900904293e4757f270e02690ff2
Rais Adli <148849921+REDGAMER551@users.noreply.github.com>
2025-01-15 13:31:29 +0800

    apprun

--- __init__.py
+++ __init__.py
@@ -1,9 +1,11 @@
 from flask import Flask, render_template, request, redirect, url_for
-from Forms import CreateUserForm, CreateCustomerForm
-import shelve, User, Customer
+import shelve
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return render_template('home.html')
+
+if __name__ == '__main__':
+    app.run()
